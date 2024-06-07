34c3a9a52f67cfd311b88c1e065fa8285a4cbd5c
Alexkor <Alexkor1303@yandex.ru>
2024-06-07 10:26:31 +0300

    final version

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ app.get("/api/persons/:id", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
+  ``;
   res.status(204).end();
 });
 
@@ -93,7 +94,7 @@ app.post("/api/persons/", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("server running at port 3001");
 });
